a63ac7541c35b72962908bd1585881f7d0d4af33
Ggxy159 <axyyu@outlook.com>
2024-01-05 17:14:21 +0800

    feat: add BlogHome.vue

---
+++ src/components/BlogHome.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1> Hi, I'm Anxcye</h1>
+    <br>
+    <h3> CS | XMU</h3>
+</template>
+
+<style scoped>
+    h1 {
+        color: #3eaf7c;
+        text-align: center;
+    }
+    h3 {
+        color: #393939;
+        text-align: center;
+    }
+</style>
